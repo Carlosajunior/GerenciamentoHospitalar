a328a5d2e2b8abc9d7e4703141fd52ed3a7b0797
Lara Esquivel <laraesquivelsantos@gmail.com>
2021-04-22 09:38:47 -0300

    Axios add

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -37,6 +37,8 @@ export default {
 }
 
 
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 createApp(App).use(router).mount('#app')
+App.use(axios,VueAxios)
