4d26fa2cbee82ead153d17a450df277619168b80
Lara Esquivel <laraesquivelsantos@gmail.com>
2021-04-25 17:52:08 -0300

    admMenuNTestado

---
+++ src/components/adm/AdmMenu.vue
@@ -0,0 +1,61 @@
+<template>
+     <input type="checkbox" id="nav-toggle">
+    <div class="sidebar">
+      <div class="sidebar-brand">
+        <h2><span class="lab la-accusoft"></span> <span>Accusoft</span>
+        </h2>
+      </div>
+
+      <div class="sidebar-menu">
+        <ul>
+          <li>
+            <a href="" class="active">
+              <span class="las la-igloo"></span><span>Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <span class="las la-users"></span><span>Costumers</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <span class="las la-clipboard-list"></span><span>Projects</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <span class="las la-shopping-bag"></span><span>Orders</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <span class="las la-receipt"></span><span>Inventory</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <span class="las la-user-circle"></span><span>Accounts</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <span class="las la-clipboard-list"></span><span>Tasks</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+
+</style>
