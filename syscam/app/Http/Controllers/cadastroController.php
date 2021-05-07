4b7a6495fc197109b0d3dedfe39b2edae8f0d758
Carlosajunior <carlosajunior@hotmail.com>
2021-05-07 01:22:38 -0300

    Criei o controller responsável pelo cadastro de usuário junto da rota

--- syscam/app/Http/Controllers/cadastroController.php
+++ syscam/app/Http/Controllers/cadastroController.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class cadastroController extends Controller
-{
-    //
+{   
+    public function criarUsuario(Request $request){
+        return $request->all();
+        // $usuario = User::create([
+        //     'nome' => $request -> nome,
+        //     'email' => $request -> email,
+        //     'senha'=> $request -> senha,
+        //     'username'=> $request -> username,
+        //     'telefone'=> $request -> telefone,
+        //     'cargo'=> $request -> cargo,
+        //     'cpf'=> $request -> cpf,
+        //     'apelido'=> $request -> apelido,
+        // ]);
+        // return $usuario;
+    }
 }
