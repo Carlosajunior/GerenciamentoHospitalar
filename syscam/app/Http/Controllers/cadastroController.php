8d5362f5b082db83c2dd670f8ab63d89719d3535
JoaoSamuel11 <43974970+JoaoSamuel11@users.noreply.github.com>
2021-05-10 14:37:10 -0300

    Revert "Merge branch 'backend' of https://github.com/laraesquivel/EngenhariaDeSoftware2021 into backend"
    This reverts commit 491a608d5d3bec5232841216585c29bd1c48117b.

--- syscam/app/Http/Controllers/cadastroController.php
+++ syscam/app/Http/Controllers/cadastroController.php
@@ -7,22 +7,8 @@ use Illuminate\Http\Request;
 class cadastroController extends Controller
 {   
     public function criarUsuario(Request $request){
-<<<<<<< Updated upstream
         return print("testando controller cadastro");
         //return $request->all();
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return $request->all();
-=======
-        return print("testando controller cadastro");
-        //return $request->all();
->>>>>>> 68fe9fcfbfd50a89141343a7ae0fc8ee1599b579
-=======
-        return print("testando controller cadastro");
-        //return $request->all();
->>>>>>> 68fe9fcfbfd50a89141343a7ae0fc8ee1599b579
->>>>>>> Stashed changes
         // $usuario = User::create([
         //     'nome' => $request -> nome,
         //     'email' => $request -> email,
--- syscam/app/Models/Medicamento.php
+++ syscam/app/Models/Medicamento.php
@@ -8,27 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Medicamento extends Model
 {
     use HasFactory;
-
-    
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'idLote',
-        'quantidade',
-        'baixaMedicamento_idBaixaMedicamento',
-        'idMedicamento',
-        'nome',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        
-    ];
 }
