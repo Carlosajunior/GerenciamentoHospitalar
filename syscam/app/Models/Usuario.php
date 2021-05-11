61a725c098a2ede7cfe99695373c27e870639f8c
Carlosajunior <carlosajunior@hotmail.com>
2021-05-11 20:02:42 -0300

    Adicionei os models que faltavam para cada tabela do banco e os relacionei com as respectivas tabelas

---
+++ syscam/app/Models/Usuario.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    protected $table = 'usuario';
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome',
+        'email',
+        'senha',
+        'telefone',
+        'cargo',
+        'cpf',
+        'apelido',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'senha',
+        'idUsuario',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
