3bcff7bf0ddca287cc38a7b467e1b902f40a8025
Carlosajunior <carlosajunior@hotmail.com>
2021-05-19 16:13:47 -0300

    implementação do log de alterações, criação do Model log, alteração da Timezone do sistema para a do Brasil e método de edição de cadastro por ID

--- syscam/app/Http/Controllers/controllerUsuario.php
+++ syscam/app/Http/Controllers/controllerUsuario.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Usuario;
+use App\Models\log;
 use Illuminate\Http\Request;
 
 class controllerUsuario extends Controller
@@ -15,36 +16,54 @@ class controllerUsuario extends Controller
 
     public function editarCadastro(Request $request)
     {
-        $usuario = Usuario::firstWhere('cpf','=',$request->cpf);
+        $usuario = Usuario::firstWhere('id','=',$request->id);
         if($usuario != null){
-            if($request -> nome != null)
+            $alteracoes= "Os dados alterados foram: ";
+            if($request -> nome != null){
+                $alteracoes = $alteracoes."o nome foi alterado de ".(String) $usuario -> nome." para ".(String) $request ->nome;
                 $usuario -> nome = $request -> nome;
-            if($request -> email != null)
+            }                
+            if($request -> email != null){
+                $alteracoes = $alteracoes." o email foi alterado de ".(String) $usuario -> email." para ".(String) $request ->email;
                 $usuario -> email = $request->email;
-            if($request -> senha != null)
+            }
+            if($request -> senha != null){
+                $alteracoes = $alteracoes." a senha foi alterada de ".(String) $usuario -> senha." para ".(String) $request ->senha;
                 $usuario -> senha = $request->senha;
-            if($request-> telefone != null)
+            }
+            if($request-> telefone != null){
+                $alteracoes = $alteracoes." o telefone foi alterado de ".(String) $usuario -> telefone." para ".(String) $request ->telefone;
                 $usuario -> telefone = $request -> telefone;
+            }
             if($request-> cpf != null){
                 $usuarioAux = Usuario::firstWhere('cpf','=',$request->cpf);
-                if($usuarioAux == null)
+                if($usuarioAux == null){
+                    $alteracoes = $alteracoes." o CPF foi alterado de ".(String) $usuario -> cpf." para ".(String) $request ->cpf;                
                     $usuario -> cpf = $request -> cpf;
+                }
                 else
                     return print('CPF inválido.');
             }
-                $usuario -> cpf = $request -> cpf;
             if($request -> apelido != null){
                 $usuarioAux = Usuario::firstWhere('apelido','=',$request->apelido);
-                if($usuarioAux == null)
+                if($usuarioAux == null){
+                    $alteracoes = $alteracoes." o apelido foi alterado de ".(String) $usuario -> apelido." para ".(String) $request ->apelido;
                     $usuario -> apelido = $request -> apelido;
+                }
                 else
                     return print('Este apelido já está em uso, escolha outro.');
-            }                
+            }
+            $alteracoes = $alteracoes.". Os dados foram alterados em ".\Carbon\Carbon::now()->toDateTimeString();
+            $log = log::create([
+                'log' => $alteracoes
+            ]);
+            $log ->save();           
             $usuario->save();
             return print('dados alterados com sucesso.');
         }
         else
             print('CPF inválido.');
+        
     }
 
     public function criarUsuario(Request $request){
