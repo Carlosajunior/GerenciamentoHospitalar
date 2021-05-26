07a51b9d1561040f1f90eab7c3afb5c36e9c2635
Lara Esquivel <laraesquivelsantos@gmail.com>
2021-05-26 10:39:22 -0300

    EnfChefMedicamentosCelll

---
+++ src/components/enfChefe/EnfChefeMedicamentosCell.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <tr>
+      <th scope="col">{{medicacao}}</th>
+      <th scope="col">{{lote}}</th>
+      <th scope="col">{{quantidade}}</th>
+      <th scope="col">{{validade}}</th>
+      <th scope="col">{{farmaceutica}}</th>
+    </tr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EnfChefeMedicamentos",
+  props: {
+    medicacao: null,
+    lote: null,
+    quantidade: null,
+    validade: null,
+    farmaceutico: null,
+  },
+};
+</script>
+
+<style>
+label {
+  margin-right: 1%;
+}
+.filtro {
+  padding: 2%;
+}
+button {
+  border: none;
+  background-color: #35b736;
+  color: white;
+  padding: 3.2px;
+  margin-left: 5%;
+}
+
+th {
+  padding: 10px;
+  text-align: center;
+}
+th[scope="col"] {
+  background-color: rgb(238, 238, 238);
+}
+</style>
