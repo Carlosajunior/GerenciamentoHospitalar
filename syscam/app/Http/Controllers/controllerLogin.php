1263777d1d4e2e8d4af91717f614382acc707421
Carlosajunior <carlosajunior@hotmail.com>
2021-06-16 21:13:31 -0300

    correção

--- syscam/app/Http/Controllers/controllerLogin.php
+++ syscam/app/Http/Controllers/controllerLogin.php
@@ -8,44 +8,9 @@ use Illuminate\Http\Request;
 class controllerLogin extends Controller
 {
     public function login(Request $request){
-        try{ 
             $apelido = $request -> apelido;
-            if($apelido  == null)
-                throw new Exception("Apelido não informado.", 400);
-           
-        }catch(Exception $e){
-            $codigoErro = $e ->getCode();
-            $mensagem = "Erro inesperado detectado. Tente novamente.";
-            if($codigoErro == 400)
-                $mensagem = $e->getMessage();
-            http_response_code($codigoErro);
-            echo $mensagem;
-        }
-        try{
-            $senha = $request -> senha;
-            if($senha  == null)
-                throw new Exception("Senha não informada.", 400);
-            
-        }catch(Exception $e){
-            $codigoErro = $e ->getCode();
-            $mensagem = "Erro inesperado detectado. Tente novamente.";
-            if($codigoErro == 400)
-                $mensagem = $e->getMessage();
-            http_response_code($codigoErro);
-            echo $mensagem;
-        }        
-        try{
-            $usuario = Usuario::firstWhere('apelido', "=", $apelido);
-            if(!$usuario)
-                throw new Exception("O apelido de usuário informado está incorreto.", 400);                
-        }catch(Exception $e){
-            $codigoErro = $e ->getCode();
-            $mensagem = "Erro inesperado detectado. Tente novamente.";
-            if($codigoErro == 400)
-                $mensagem = $e->getMessage();
-            http_response_code($codigoErro);
-            echo $mensagem;
-        }
+            $senha = $request -> senha;            
+            $usuario = Usuario::firstWhere('apelido', "=", $apelido);           
         try{
             if($usuario && $usuario->senha == $senha){
                 $token = $usuario->createToken($apelido)->plainTextToken;
@@ -56,7 +21,7 @@ class controllerLogin extends Controller
                 return response($response);
             }
             else
-                throw new Exception("Senha incorreta.", 400);                
+                throw new Exception("Credenciais inválidas.", 400);                
         }catch(Exception $e){
             $codigoErro = $e ->getCode();
             $mensagem = "Erro inesperado detectado. Tente novamente.";
