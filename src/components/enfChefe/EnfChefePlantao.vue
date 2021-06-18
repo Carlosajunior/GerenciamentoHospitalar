15ebfe933a828e25fab80123c00179ec3899388d
biancasilva2344 <57757405+biancasilva2344@users.noreply.github.com>
2021-06-18 14:47:42 -0300

    plantão

---
+++ src/components/enfChefe/EnfChefePlantao.vue
@@ -0,0 +1,9 @@
+<template>
+      <button button  @submit.prevent="postForm" type="submit" class="b-salvar">Entrar em plantão</button>
+      <button class="b-cancelar">Sair do plantão</button>
+</template>
+
+<script>
+
+
+</script>
--- src/enfChefeView/HomeEnfChefe.vue
+++ src/enfChefeView/HomeEnfChefe.vue
@@ -3,21 +3,25 @@
         <enf-chefe-menu>
         </enf-chefe-menu>
     </div>
+    <div>
+        <enf-chefe-plantao> 
+            
+        </enf-chefe-plantao>
+    </div>
     <div id ="main-content">
         <enf-chefebar :title="'Home'" kindUser="Enfermeiro Chefe">
         </enf-chefebar>
-        <img class="img-fluid" src="../assets/enfCapa.jpg">
-
     </div>
-        
+        <img class="img-fluid" src="../assets/enfCapa.jpg">
  
 </template>
 
 <script>
 import EnfChefeMenu from '../components/enfChefe/EnfChefeMenu.vue'
 import EnfChefebar from '../components/adm/AdmBar.vue'
+import EnfChefePlantao from '../components/enfChefe/EnfChefePlatao.vue'
 export default {
-    components:{EnfChefeMenu, EnfChefebar} 
+    components:{EnfChefeMenu, EnfChefebar, EnfChefePlantao } 
 }
 </script>
 
@@ -28,7 +32,13 @@ export default {
   width: 95px;
 }
 .img-fluid{
+    margin-left: 345px;
     padding: 10%;
+    width: 70%;
+    padding-top: 12%;
+    padding-left: 8%;
+    padding-right: 8%;
+    padding-bottom: 8%;
 }
 
 </style>
