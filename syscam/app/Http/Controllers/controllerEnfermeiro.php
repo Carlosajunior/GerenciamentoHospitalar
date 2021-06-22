6e7fa8dffec7b32568bbc0b3f5cb4ebf561f85e7
Danrlei Almeida Araujo <danrleiaraujo@gmail.com>
2021-06-21 23:54:34 -0300

    Mudanças no controller do enfermeiro

--- syscam/app/Http/Controllers/controllerEnfermeiro.php
+++ syscam/app/Http/Controllers/controllerEnfermeiro.php
@@ -10,7 +10,8 @@ use Illuminate\Http\Request;
 
 class controllerEnfermeiro extends Controller
 {
-    public function cadastrarPacienteEnf(Request $request){   //Cadastra um novo Paciente 
+    //Cadastra um novo Paciente 
+    public function cadastrarPacienteEnf(Request $request){   
         $paciente = new Paciente;
         $paciente = Paciente::create([
             'nome' => $request -> nome,
@@ -26,26 +27,22 @@ class controllerEnfermeiro extends Controller
         return $paciente;
     } 
     //---------------------------------------------------------------------------------
-    public function EstoqueMedicamentos(){ //Gera uma lista com os Medicamentos em estoque 
+    //Gera uma lista com os Medicamentos em estoque 
+    public function EstoqueMedicamentos(){
         $array = collect([]);
         foreach (Medicamento::all() as $Medicamento)
             $array->push($Medicamento);
         return($array);
     }
     //---------------------------------------------------------------------------------
-    public function ListarPacientes(){ //Gera uma lista com os Pacientes castrados 
+    //Gera uma lista com os Pacientes castrados 
+    public function ListarPacientes(){ 
         $array = collect([]);
         foreach (Paciente::all() as $Paciente)
             $array->push($Paciente);
         return($array);
     }
-    //---------------------------------------------------------------------------------
-    public function verificarAgendamento (){  //Gera uma lista com os Agendamentos castrados 
-        $array = collect([]);
-        foreach (agendamento_medicacao::all() as $agendamento_medicacao)
-            $array->push($agendamento_medicacao);
-        return($array);
-    }    
+
     //----------------------------------------------------------------------------------
     public function emitirRelatorioPaciente(){ // Retorna a lista de pacientes e a lista de prontuario
         $arrayPaciente = collect([]);
@@ -67,16 +64,6 @@ class controllerEnfermeiro extends Controller
         if($preparo != null){          
             if($request -> quantidade != null)
             $preparo -> quantidade = $request -> quantidade;
-
-        /*Fiz mas acho que não precisa
-            if($request -> lote != null)
-                $preparo -> lote = $request -> lote; 
-            if($request -> nome != null)
-                $preparo -> nome = $request -> nome;
-            if($request -> id_baixaMedicamento != null)
-                $preparo -> id_baixaMedicamento = $request -> id_baixaMedicamento;                
-        */
-
         }
         else {
             return print('Medicamento inválido.');
@@ -85,7 +72,8 @@ class controllerEnfermeiro extends Controller
     }
 
     //-----------------------------------------------------------------------------------
-    public function baixarAgendamento(Request $request){ //Gera um novo agendamento
+    //Muda o estado do agendamento para "feito":
+    public function baixarAgendamento(Request $request){ 
         $novaBaixa = new agendamento_medicacao;
         $novaBaixa = agendamento_medicacao::firstWhere('id','=',$request->id);
         if($novaBaixa != null){          
@@ -93,9 +81,19 @@ class controllerEnfermeiro extends Controller
             $novaBaixa -> feito = $request -> feito;
         }
     }
-    
+        //---------------------------------------------------------------------------------
+    //Retorna um array com todos os Agendamentos tendo como entrada o id_usuario do enfermeiro logado:
+    public function verificarAgendamento (Request $request){  
+        $array = collect([]);
+        foreach (agendamento_medicacao::all() as $agendamento_medicacao){
+            if($request ->id_usuario == $agendamento_medicacao->id_usuario)
+                $array->push($agendamento_medicacao);
+        }
+        return($array);
+    }    
     //-----------------------------------------------------------------------------------
-    public function historicoAgendamentos(Request $request){ // Retorna um array com agendamentos já feitos pelo enfermeiro
+    // Retorna um array com agendamentos já feitos pelo enfermeiro logado através do id_usuario:
+    public function historicoAgendamentos(Request $request){ 
         $array = collect([]);
         foreach (Agendamento_medicacao::all() as $Agendamento_medicacao)
             if($Agendamento_medicacao -> id_usuario == $request -> id_usuario  && $Agendamento_medicacao -> feito == 1)
@@ -104,14 +102,13 @@ class controllerEnfermeiro extends Controller
     }
 
     //-----------------------------------------------------------------------------------------
-
-    public function AgendamentosPendentes(){ //Retorna um array com agendamentos pendentes
+    //Retorna um array com agendamentos pendentes para o enfermeiro logado através do id_usuario:
+    public function AgendamentosPendentes(Request $request){ 
         $array = collect([]);
         foreach (Agendamento_medicacao::all() as $Agendamento_medicacao)
-            if($Agendamento_medicacao -> feito == 0)
+            if($Agendamento_medicacao -> id_usuario == $request -> id_usuario  && $Agendamento_medicacao -> feito == 0)
                 $array->push($Agendamento_medicacao);
         return($array);
     }
-
     //------------------------------------------------------------------------------------------
 }
