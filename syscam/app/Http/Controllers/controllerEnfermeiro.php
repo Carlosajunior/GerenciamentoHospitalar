aef906bef48d736c24f4b397f580ea66a0d01532
carlosajunior <carlosajunior@hotmail.com>
2021-06-22 18:25:22 -0300

    rota de agendamentos que o enf/esta falta concluir

--- syscam/app/Http/Controllers/controllerEnfermeiro.php
+++ syscam/app/Http/Controllers/controllerEnfermeiro.php
@@ -106,9 +106,10 @@ class controllerEnfermeiro extends Controller
     //Retorna um array com agendamentos pendentes para o enfermeiro logado através do id_usuario:
     public function AgendamentosPendentes(Request $request){ 
         $array = collect([]);
-        foreach (Agendamento_medicacao::all() as $Agendamento_medicacao)
-            if($Agendamento_medicacao -> id_usuario == $request -> id_usuario  && $Agendamento_medicacao -> feito == 0)
-                $array->push($Agendamento_medicacao);
+        foreach (Agendamento_medicacao::where('id_usuario',$request->id)->get() as $agendamento_medicacao){
+            if($agendamento_medicacao->feito == false)
+                $array->push($agendamento_medicacao);
+        }
         return($array);
     }
     //------------------------------------------------------------------------------------------
